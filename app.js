53a03ef3ba59354a02e09603182e7f0f603fd79e
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-24 17:37:16 +0200

    Add database.connect function call to app.js.
    Open up connection there once.

--- app.js
+++ app.js
@@ -8,5 +8,12 @@ app.use(express.json());
 app.use('/api', todos);
 
 const server = app.listen(8080, async () => {
-  console.log(`Listening on port ${server.address().port}`);
+  try {
+    await database.connect();
+    console.log('Database connection successful');
+    console.log(`Listening on port ${server.address().port}`);
+  } catch (err) {
+    console.log(err);
+    server.close();
+  }
 });
