1690f20cd28cb4452db9047fb18f85f5265517b1
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-12-01 14:36:32 +0200

    Remove quotes from config settings.

--- database/config.js
+++ database/config.js
@@ -1,8 +1,8 @@
 const config = {
-  host: 'process.env.HOST',
-  user: 'process.env.USER',
-  password: 'process.env.PASSWORD',
-  database: 'process.env.DATABASE',
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   connectionLimit: 10,
   dateStrings: true,
 };
