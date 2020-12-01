b56a1b3c2b653e6a2543e17f329ce1707b954188
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-12-01 12:29:30 +0200

    Resolve conflicts.

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-app.use(cors());
 
-const db = [{ name: 'tiina' }, { name: 'jack' }];
+// const database = require('./database/crudrepository.js');
+const dbConnection = require('./database/connection');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const todos = require('./routes/todos.js');
+const lists = require('./routes/lists');
+
 const port = process.env.PORT || 8080;
 
-app.get('/names', (req, res) => {
-  res.send(db);
+if (!process.env.PORT) {
+  app.use(cors());
+}
+app.use(express.static('frontend/build'));
+app.use(express.json());
+app.use('/api', todos);
+app.use('/api', lists);
+
+const server = app.listen(port, async () => {
+  try {
+    // PROBLEM: this doesn't really try connecting to db...
+    // if db server not running its "successful" in connecting
+    await dbConnection.connect();
+    console.log('Database connection successful');
+    console.log(`Listening on port ${server.address().port}`);
+  } catch (err) {
+    console.log(err);
+    server.close();
+  }
 });
 
-const server = app.listen(port, () => {
-  console.log(`Listening on port ${server.address().port}`);
+// Graceful exit
+process.on('SIGINT', function () {
+  console.log('Closing...');
+  dbConnection.close();
+  process.exit();
 });
